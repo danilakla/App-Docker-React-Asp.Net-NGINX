ca4d34ff3be2a0fa18cc2130cd48665b1c420e31
danila <danila.kla@mail.ru>
2022-12-25 12:16:41 +0300

    Добавьте файлы проекта.

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult GetUser()
+    {
+        var users = new[]{
+             new { Name = "Danila" },
+                          new { Name = "Test" }
+        };
+        return Ok(users);   
+    }
+}
